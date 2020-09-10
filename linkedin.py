4f1a420d3bfcf26608c50092c5a1d015e03ef5f9
fulcus <francescofgonzales@gmail.com>
2020-09-10 17:15:17 +0200

    Implemented sheets

--- linkedin.py
+++ linkedin.py
@@ -3,8 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import xlrd
+from xlutils.copy import copy
 
 driver = webdriver.Chrome()
 
@@ -27,6 +28,7 @@ class Company:
 
     def __init__(self, company_name):
         self.name = company_name
+
         driver.get(
             'https://www.linkedin.com/search/results/companies/?keywords=' + company_name)
 
@@ -36,15 +38,12 @@ class Company:
 
         # click on about
         about_selector = 'ul.org-page-navigation__items li:nth-child(2)'
-        try:
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, about_selector)))
-            driver.find_element_by_css_selector(
-                'ul.org-page-navigation__items li:nth-child(2)').click()
-        except TimeoutException:
-            print("Loading took too much time!")
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, about_selector)))
+        driver.find_element_by_css_selector(
+            'ul.org-page-navigation__items li:nth-child(2)').click()
 
-    # get description
+
     def get_description(self):
         try:
             WebDriverWait(driver, 20).until(
@@ -54,8 +53,8 @@ class Company:
             return description
         except TimeoutException:
             print("Loading took too much time!")
+            return None
 
-    # get website
     def get_website(self):
         website_xpath = "//dt[text()='Website']/following-sibling::dd"
         try:
@@ -65,20 +64,20 @@ class Company:
             return website
         except TimeoutException:
             print("Loading took too much time!")
+            return None
 
-    # get nr employees
     def get_nr_employees(self):
         nr_emp_class = "org-about-company-module__company-size-definition-text"
         try:
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, nr_emp_class)))
             nr_emp = driver.find_element_by_class_name(nr_emp_class).text
-            nr_emp = nr_emp.replace(" employees","")
+            nr_emp = nr_emp.replace(" employees", "")
             return nr_emp
         except TimeoutException:
             print("Loading took too much time!")
+            return None
 
-    # get headquarters
     def get_headquarters(self):
         hq_xpath = "//dt[text()='Headquarters']/following-sibling::dd"
         try:
@@ -88,28 +87,54 @@ class Company:
             return hq
         except TimeoutException:
             print("Loading took too much time!")
+            return None
 
 
+# open excel
+rb = xlrd.open_workbook("companies.xlsx", formatting_info=True)
+r_sheet = rb.sheet_by_index(0) # read only copy to introspect the file
+wb = copy(rb) # a writable copy (I can't read values out of this, only write to it)
+w_sheet = wb.get_sheet(0) # the sheet to write to within the writable copy
 
+# login linkedin
+login("francescofgonzales@gmail.com", "gradiente")
 
+# ROWS: 3 to 92 (excluded) in python (4 to 92 excel)
+for row in range(3, 5):
 
-# To open Workbook 
-# wb = xlrd.open_workbook("companies.xlsx") 
-# sheet = wb.sheet_by_index(0) 
+    # COLUMNS: 2 name, 3 website, 4 HQ, 5 state, 6 description, 7 employees, 8 others
+    NAME_COL = 2
+    WEB_COL = 3
+    HQ_COL = 4
+    HQ_COUNTRY_COL = 5
+    DESCR_COL = 6
+    EMPL_COL = 7
 
-#print(sheet.cell_value(0, 1)) 
+    # get company name
+    company_name = r_sheet.cell_value(row, NAME_COL)
+    try:
+        company = Company(company_name)
+    except NoSuchElementException:
+        print("Couldn't find company")
+    except: 
+        print("Error searching company")
 
-#ROWS: 3 to 91 in python (4 to 92 excel)
-#COLUMNS: 2 name, 3 website, 4 HQ, 5 state, 6 description, 7 employees, 8 others 
+    # write data to sheet
+    web = company.get_website()
+    w_sheet.write(row, WEB_COL, web)
 
+    hq = company.get_headquarters()
+    w_sheet.write(row, HQ_COL, hq)
 
-#login linkedin
-login("francescofgonzales@gmail.com","gradiente")
+    descr = company.get_description()
+    w_sheet.write(row, DESCR_COL, descr)
 
-google = Company("google")
-# print(google.get_description())
-# print(google.get_headquarters())
-print(google.get_nr_employees())
-# print(google.get_website())
+    empl = company.get_nr_employees()
+    w_sheet.write(row, EMPL_COL, empl)
+
+file_path = 'w_companies'
+#wb.save(file_path + '.out' + os.path.splitext(file_path)[-1])
+wb.save(file_path + '.xlsx')
+
 
 driver.quit()
